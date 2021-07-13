d04db78cac615ea7d785b189d2dc4baeea74c48d
kunalaski <47791109+kunalaski@users.noreply.github.com>
2021-07-13 18:50:35 +0530

    final commit

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 	$db_host='sql6.freemysqlhosting.net';
 	$db_user='sql6424870';
 	$db_pass='hWktF4FaMr';
-	$db_name='credit';
+	$db_name='sql6424870';
 
 	try {
 		$pdo= new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_pass);
